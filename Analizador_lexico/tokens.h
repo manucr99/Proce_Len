bf4af540a96ef36df19550f8a16502f50009979e
Yunhao <yunhi.0799@gmail.com>
2020-10-17 15:59:50 +0200

    cambios

---
+++ Analizador_lexico/tokens.h
@@ -0,0 +1,39 @@
+/**
+ * @file tokens.h
+ * @author Yunhao Lin, Jesus Alvarez, Manuel 
+ * @brief Este fichero contiene las declaraciones de tokens que posteriormente
+ *         sera usado por el analizador lexico
+ * @version 0.1
+ * @date 2020-10-17
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef __TOKENS_H
+#define __TOKENS_H
+
+#define CARACTERES     256
+#define NUMERO         257
+#define INI_BLOQUE     258
+#define FIN_BLOQUE     259
+#define INI_AGREGADO   260
+#define FIN_AGREGADO   261
+#define PUNTOYCOMA     262
+#define OP_ASIGNACION  263
+#define BUCLE_SI       264
+#define OP_ARITMETICA  265
+#define BUCLE_MIENTRAS 266
+#define OP_LOGICO      267
+#define OP_UNARIO      268
+#define BUCLE_PARA     269
+#define INI_PARENTESIS 270
+#define FIN_PARENTESIS 271
+#define SINO           272
+#define ENTONCES       273
+#define DECL_LISTAS    274
+#define OP_UNI_BIN     275
+#define TIPO_VAR       276
+#define ID             277
+
+#endif
